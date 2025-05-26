977c8afc209fd835bfc8ab61a18fdccb1d220752
Freezingle <bct078011@student.ioepc.edu.np>
2025-05-26 08:32:05 +0545

    attack method prototype done

--- client/classes.js
+++ client/classes.js
@@ -1,42 +1,55 @@
 class Character {
-        constructor (x,y,color,width,height,id){
-            this.x = x;
-            this.y = y;
-            this.width =width;
-            this.height = height;
-            this.color=  color;
-            this.id = id;
-            this.isAttacking = false;
-            this.attackBox = {
-                position:{x:this.x, y:this.y},
-                width: 0,
-                height: 0,
-                offsetY:0,  
-                offsetDirection: 1,  // 1 for right, -1 for left 
-                 offsetX: 0
-            };
-            this.attackBox.offsetX = this.width * this.attackBox.offsetDirection;
-        }
-        draw(ctx) {
-            ctx.fillStyle = this.color;
-            ctx.fillRect(this.x, this.y, this.width,this.height);
-            // Draw attack box if attacking
-            if  (this.isAttacking ){
-                 this.attackBox.offsetX = this.width * this.attackBox.offsetDirection;
-                console.log("Drawing attack box");
-                 ctx.fillStyle = "grey";
-        ctx.fillRect(this.attackBox.position.x+ this.attackBox.offsetX, this.attackBox.position.y+this.attackBox.offsetY, this.attackBox.width, this.attackBox.height);
-            }
-        }
-        move(keys) {
-            if (keys['d']) this.x += this.speed;
-            if (keys["a"]) this.x -=  this.speed;
-            if (keys["w"]) this.y -= this.speed;
-            if (keys["s"]) this.y += this.speed;      
-            // Update attack box position
+    constructor(x, y, color, width, height, id) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.id = id;
+        this.isAttacking = false;
+        this.facingDirection = 1; // 1 = right, -1 = left
+
+        this.attackBox = {
+            position: { x: this.x, y: this.y },
+            width: 0,
+            height: 0,
+            offsetY: 0,
+            offsetX: this.width // will be negated if facing left
+        };
+    }
+
+    updateAttackDirection(opponentX) {
+        this.facingDirection = opponentX < this.x ? -1 : 1;
+        this.attackBox.offsetX = this.facingDirection === 1 ? this.width : -this.attackBox.width;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        if (this.isAttacking) {
             this.attackBox.position.x = this.x;
             this.attackBox.position.y = this.y;
-        }   
+
+            ctx.fillStyle = "grey";
+            ctx.fillRect(
+                this.attackBox.position.x + this.attackBox.offsetX,
+                this.attackBox.position.y + this.attackBox.offsetY,
+                this.attackBox.width,
+                this.attackBox.height
+            );
+        }
+    }
+
+    move(keys) {
+        if (keys['d']) this.x += this.speed;
+        if (keys['a']) this.x -= this.speed;
+        if (keys['w']) this.y -= this.speed;
+        if (keys['s']) this.y += this.speed;
+
+        this.attackBox.position.x = this.x;
+        this.attackBox.position.y = this.y;
+    }
 }
 
 class Bruiser extends Character {
@@ -46,7 +59,6 @@ class Bruiser extends Character {
         this.type = "Bruiser";
         this.attackBox.width = 50; // Wider attack box
         this.attackBox.height = 30; // Taller attack box
-        console.log(`${this.width} ${this.height}`);
     }
     attack (ctx){
         if (this.isAttacking) return; // Prevent multiple attacks at once
@@ -89,4 +101,4 @@ class Assassin extends Character {
       // Deactivate attack box here
     }, 500);
     }
-}
+} 
