bae2894be5df913c56d658d1b39b07fcbf0bb81f
Freezingle <bct078011@student.ioepc.edu.np>
2025-07-06 15:43:14 +0545

    hardfixed loggedIn issue by setting it to false on load

--- server/config.js
+++ server/config.js
@@ -1,29 +1,29 @@
-    require('dotenv').config();
-  const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const dbUri = process.env.DB_URI; // This gets the value from .env
 
 const connect = mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    //check connection
-    connect.then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch ((err)=>{
-        console.log("Error connecting to MongoDB:", err);
-    })
+// creating a schema for user
+const LoginSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    isLoggedIn: { type: Boolean, default: false },
+    tokens: { type: Number, default: 0 },
+    xpLevel: { type: Number, default: 1 },
+    gamesPlayed: { type: Number, default: 0 },
+    wins: { type: Number, default: 0 }
+});
 
-    //creating a schema for user
-    const LoginSchema = new mongoose.Schema({
-        name:{type: String,required: true},
-        password: {type:String,required: true},
-        isLoggedIn: {type: Boolean, default: false},
-        tokens: {type:Number, default:0},
-        xpLevel: {type:Number, default:1},
-        gamesPlayed: {type:Number, default:0},
-        wins: {type:Number, default:0}
-    })
+// creating a collection model for user
+const collection = mongoose.model("users", LoginSchema);
 
-    //creating a collectio modelfor user
-    const collection = new mongoose.model("users", LoginSchema);
+connect.then(async () => {
+    console.log("Connected to MongoDB");
+    const result = await collection.updateMany({}, { $set: { isLoggedIn: false } });
+    console.log("All users set to not logged in");
+}).catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+});
 
-    module.exports =  collection;
+module.exports = collection;
