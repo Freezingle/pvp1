8dbaca17156eef8892f2419dce633c20a4251896
Freezingle <bct078011@student.ioepc.edu.np>
2025-07-14 11:46:32 +0545

    minor changes

--- server/server.js
+++ server/server.js
@@ -169,12 +169,11 @@ io.on("connection", (socket) => {
     socket.to(roomId).emit("newPlayer", { id: socket.id, ...player });
   });
 
-  socket.on("playerUpdate", ({ roomId, x, y, hitsLanded, facingDirection }) => {
+  socket.on("playerUpdate", ({ roomId, x, y, hitsLanded }) => {
     if (rooms[roomId] && rooms[roomId][socket.id]) {
       rooms[roomId][socket.id].x = x;
       rooms[roomId][socket.id].y = y;
       rooms[roomId][socket.id].hitsLanded = hitsLanded;
-      rooms[roomId][socket.id].facingDirection = facingDirection;
 
       io.to(roomId).emit("opponentMove", { id: socket.id, x, y, hitsLanded });
     }
